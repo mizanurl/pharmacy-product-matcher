0bcdb31d9f75b665ac401120272c73bcc71501e7
mizanurl <cicakemizan@gmail.com>
2024-07-26 07:56:32 +0600

    Removed dead code
    Removed dead code from the service file

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { parseCSV } from '../utils/csvParser.js';
 import Product from '../models/product.js';
-//import { createObjectCsvStringifier } from 'csv-writer';
 
 const files = [
     'apo-lt-data.csv',
@@ -76,45 +75,3 @@ export const getMatchedProducts = async () => {
 
     return matchedProducts;
 };
-
-/*export const exportMatchedProductsToCSV = async (matchedProducts, filePath) => {
-    const csvWriter = createObjectCsvWriter({
-        path: filePath,
-        header: [
-            {id: 'main_product_title', title: 'main_product_title'},
-            {id: 'main_manufacturer', title: 'main_manufacturer'},
-            {id: 'main_source', title: 'main_source'},
-            {id: 'main_source_id', title: 'main_source_id'},
-            {id: 'competitor_product_title', title: 'competitor_product_title'},
-            {id: 'competitor_manufacturer', title: 'competitor_manufacturer'},
-            {id: 'competitor_source', title: 'competitor_source'},
-            {id: 'competitor_source_id', title: 'competitor_source_id'}
-        ]
-    });
-
-    let records = [];
-
-    matchedProducts.forEach(matchedProduct => {
-        const mainProduct = matchedProduct.mainProduct;
-        matchedProduct.competitorProducts.forEach(competitorProduct => {
-
-            let competitorProductTitle = ( competitorProduct !== undefined && competitorProduct.title !== undefined ) ? competitorProduct.title : '';
-            let competitorProductManufacturer = ( competitorProduct !== undefined && competitorProduct.manufacturer !== undefined ) ? competitorProduct.manufacturer : '';
-            let competitorProductSource = ( competitorProduct !== undefined && competitorProduct.source !== undefined ) ? competitorProduct.source : '';
-            let competitorProductSourceId = ( competitorProduct !== undefined && competitorProduct.source_id !== undefined ) ? competitorProduct.source_id : '';
-
-            records.push({
-                main_product_title: mainProduct.title,
-                main_manufacturer: mainProduct.manufacturer,
-                main_source: mainProduct.source,
-                main_source_id: mainProduct.source_id,
-                competitor_product_title: competitorProductTitle,
-                competitor_manufacturer: competitorProductManufacturer,
-                competitor_source: competitorProductSource,
-                competitor_source_id: competitorProductSourceId
-            });
-        });
-    });
-
-    await csvWriter.writeRecords(records);
-};*/
